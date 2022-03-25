f9dd3bc5fc57e91e85dea40a4e8aa4fbb78eb6bc
MAbdurahman <mdbdrrhm@gmail.com>
2022-03-25 03:39:28 -0500

    Add DefaultLayout.jsx

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from 'antd';
+
 
 export default function App() {
   return (
     <div>
-      <h2>app component</h2>
+      <Button type='primary'>App Component</Button>
     </div>
   )
 }
