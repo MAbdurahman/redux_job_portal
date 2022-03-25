894927513128dd02d94e1ce09444d4b5b16e4782
MAbdurahman <mdbdrrhm@gmail.com>
2022-03-25 06:00:06 -0500

    Connect backend/server.js to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,17 @@
 const app = require('./app/app');
 const dotenv = require('dotenv');
 const colors = require('colors');
-// const connectDatabase = require('./config/databaseConfig');
+const connectDatabase = require('./config/databaseConfig');
 
 //**************** configuration setup ****************//
 dotenv.config({ path: 'backend/config/config.env' });
-
+colors.enable();
 //**************** variables ****************//
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV;
 
 //**************** connect to database ****************//
-// connectDatabase();
+connectDatabase();
 
 //**************** app listening ****************//
 const server = app.listen(PORT, () => {
